024fa358f179acb4bdac3e21b91dc33f67b67489
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-02 13:55:07 -0800

    Create tests

---
+++ test/auction_test.rb
@@ -0,0 +1,202 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+class AuctionTest < Minitest::Test
+
+  def test_it_exists
+    auction = Auction.new
+
+    assert_instance_of Auction, auction
+  end
+
+  def test_it_has_no_items
+    auction = Auction.new
+
+    assert_equal [], auction.items
+  end
+
+  def test_it_can_add_items
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    assert_equal [item1, item2], auction.items
+  end
+
+  def test_it_can_list_item_names
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    expected = ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
+
+    assert_equal expected, auction.item_names
+  end
+
+  def test_it_has_no_bids
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    expected = {
+    }
+
+    assert_equal expected, item1.bids
+  end
+
+  def test_it_can_add_bids
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expected = {
+      attendee2 => 20,
+      attendee1 => 22
+    }
+
+    assert_equal expected, item1.bids
+  end
+
+  def test_it_has_a_current_high_bid
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    assert_equal 22, item1.current_high_bid
+  end
+
+  def test_it_has_unpopular_items
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+
+    expected = [item2, item3, item5]
+
+    assert_equal expected, auction.unpopular_items
+
+    item3.add_bid(attendee2, 15)
+
+    expected = [item2, item5]
+
+    assert_equal expected, auction.unpopular_items
+  end
+
+  def test_it_can_calculate_potential_revenue
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    assert_equal 87, auction.potential_revenue
+  end
+
+  def test_it_can_list_bidders
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    expected = ["Megan", "Bob", "Mike"]
+
+    assert_equal expected, auction.bidders
+  end
+end
